e8a01dac50c405a3632bc757e17cefff1905b7d1
unknown <fournier.phk@gmail.com>
2019-05-20 22:51:33 -0400

    training plots

--- agents/train.py
+++ agents/train.py
@@ -4,75 +4,94 @@ if __name__ == '__main__':
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.99, neurons=[8, 8, 8])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.99, neurons=[16, 8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.99, neurons=[8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.99, neurons=[16, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.99, neurons=[8])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.99, neurons=[8, 8, 8])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.97, neurons=[16, 8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.97, neurons=[8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.999, gamma=0.97, neurons=[16, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.99, neurons=[8, 8, 8])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.99, neurons=[16, 8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.99, neurons=[8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.99, neurons=[16, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.99, neurons=[8])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.99, neurons=[8, 8, 8])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.97, neurons=[16, 8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.97, neurons=[8, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.97, neurons=[16, 4])
     agent.play()
     agent.save_model()
+    agent.plot_model()
 
     agent = CartpoleAgent(episodes=10000, epsilon_decay=0.995, gamma=0.97, neurons=[8])
     agent.play()
     agent.save_model()
+    agent.plot_model()
